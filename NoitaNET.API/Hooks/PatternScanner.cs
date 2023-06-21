ba188350b118c92b42888c1d3d9b232984a0de96
chair <projectaa76@gmail.com>
2023-06-21 02:48:21 -0400

    Add `PatternScanner` API

---
+++ NoitaNET.API/Hooks/PatternScanner.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace NoitaNET.API.Hooks;
+
+public static class PatternScanner
+{
+    public static unsafe nint Scan(ProcessModule module, string pattern)
+    {
+        string[] splits = pattern.Split(' ');
+
+        bool[] mask = new bool[splits.Length];
+        byte[] bytePattern = new byte[splits.Length];
+
+        for (int i = 0; i < splits.Length; i++)
+        {
+            string s = splits[i];
+            if (s.Length != 2)
+            {
+                throw new ArgumentException($"Invalid pattern byte at {i}", pattern);
+            }
+
+            if (s == "??")
+            {
+                mask[i] = true;
+            }
+            else
+            {
+                if (byte.TryParse(s, NumberStyles.HexNumber, null, out byte result))
+                {
+                    bytePattern[i] = result;
+                }
+                else
+                {
+                    throw new ArgumentException($"Invalid pattern byte at {i}", pattern);
+                }
+            }
+        }
+
+        return Scan(module, mask, bytePattern);
+    }
+
+    public static unsafe nint Scan(ProcessModule module, bool[] mask, byte[] bytePattern)
+    {
+        nint baseAddress = module.BaseAddress;
+        nint endAddress = module.BaseAddress + module.ModuleMemorySize;
+
+        for (nint i = baseAddress; i < endAddress; i++)
+        {
+            try
+            {
+                Span<byte> read = new Span<byte>((void*)i, bytePattern.Length);
+
+                bool found = false;
+
+                for (int j = 0; j < read.Length; j++)
+                {
+                    found = mask[j] || read[j] == bytePattern[j];
+
+                    if (!found)
+                        break;
+                }
+
+                if (found)
+                {
+                    return i;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        return -1;
+    }
+}
--- NoitaNET.TestMod/TestMod.cs
+++ NoitaNET.TestMod/TestMod.cs
@@ -1,4 +1,8 @@
-﻿using NoitaNET.API;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using NoitaNET.API;
+using NoitaNET.API.Hooks;
 using NoitaNET.API.Logging;
 
 namespace NoitaNET.TestMod;
@@ -24,23 +28,23 @@ public class TestMod : Mod
     {
         UpdateCount++;
 
-        RawEngineAPI.EntityGetWithTag("player_unit", out nint[] tempEntities);
+        //RawEngineAPI.EntityGetWithTag("player_unit", out nint[] tempEntities);
 
-        if (tempEntities.Length != 1) return;
+        //if (tempEntities.Length != 1) return;
 
-        RawEngineAPI.EntityGetTransform(tempEntities[0], out double x, out double y, out _, out _, out _);
+        //RawEngineAPI.EntityGetTransform(tempEntities[0], out double x, out double y, out _, out _, out _);
 
-        RawEngineAPI.EntityGetInRadius(x, y, 40, out nint[] entitiesInRadius);
+        //RawEngineAPI.EntityGetInRadius(x, y, 40, out nint[] entitiesInRadius);
 
-        foreach (nint entity in entitiesInRadius)
-        {
-            RawEngineAPI.EntityHasTag(entity, "player_unit", out bool isPlayer);
+        //foreach (nint entity in entitiesInRadius)
+        //{
+        //    RawEngineAPI.EntityHasTag(entity, "player_unit", out bool isPlayer);
 
-            if (isPlayer)
-                continue;
+        //    if (isPlayer)
+        //        continue;
 
-            RawEngineAPI.EntityInflictDamage(entity, 1, "slice", "Test kill", "NORMAL", 0, 0);
-        }
+        //    RawEngineAPI.EntityInflictDamage(entity, 1, "slice", "Test kill", "NORMAL", 0, 0);
+        //}
 
         if (!RunOnce && UpdateCount % BenchmarkEveryNFrames == 0)
         {
